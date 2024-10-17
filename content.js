58eb5ea4210daf72b8ee8f2e7d50948bf74045f3
Dits Eduard <rdiggs080@gmail.com>
2024-10-17 23:17:21 +0300

    последний коммит перед большим обновлением

--- content.js
+++ content.js
@@ -1,16 +1,20 @@
 // Данные для автозаполнения
 const autofillData = {
     repository: "https://gitlab.wildberries.ru/content/content-card",
-    tokenPurpose: "Расширение токена необходимо для управления новым атрибутом app-2-app"
+    tokenPurpose: "Расширение токена необходимо для управления новым атрибутом app-2-app",
+    scopeList: "\"contentAdmin\""
 };
 
 // Функция для заполнения полей формы
 function fillFields() {
+    // Создаем и инициируем событие input, чтобы система обработала изменения
+    const inputEvent = new Event('input', { bubbles: true });
+
     // Поле 'Репозиторий сервиса'
     const repositoryField = document.getElementById(":rq:");
     if (repositoryField) {
         repositoryField.value = autofillData.repository;
-        repositoryField.dispatchEvent(new Event('input')); // Программное событие, чтобы поле "заметило" изменение
+        repositoryField.dispatchEvent(inputEvent); // Программное событие, чтобы система обработала изменение
         console.log("Поле 'Репозиторий сервиса' заполнено автоматически.");
     } else {
         console.log("Поле 'Репозиторий сервиса' не найдено.");
@@ -20,22 +24,22 @@ function fillFields() {
     const tokenPurposeField = document.getElementById(":rm:");
     if (tokenPurposeField) {
         tokenPurposeField.value = autofillData.tokenPurpose;
-        tokenPurposeField.dispatchEvent(new Event('input')); // Программное событие, чтобы поле "заметило" изменение
+        tokenPurposeField.dispatchEvent(inputEvent); // Программное событие, чтобы система обработала изменение
         console.log("Поле 'Назначение токена' заполнено автоматически.");
     } else {
         console.log("Поле 'Назначение токена' не найдено.");
     }
+
+    // Поле 'Список Scope'ов (пространств)'
+    const scopeListField = document.getElementById(":rr:");
+    if (scopeListField) {
+        scopeListField.value = autofillData.scopeList;
+        scopeListField.dispatchEvent(inputEvent); // Программное событие, чтобы система обработала изменение
+        console.log("Поле 'Список Scope'ов (пространств)' заполнено автоматически.");
+    } else {
+        console.log("Поле 'Список Scope'ов (пространств)' не найдено.");
+    }
 }
 
-// Наблюдатель для отслеживания изменений в DOM
-const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        fillFields(); // Вызываем заполнение, если произошли изменения
-    });
-});
-
-// Настраиваем наблюдение за изменениями в DOM
-observer.observe(document.body, { childList: true, subtree: true });
-
-// Добавляем начальную задержку для полной загрузки страницы
+// Добавляем задержку, чтобы убедиться, что все поля полностью загружены
 setTimeout(fillFields, 3000); // Задержка 3 секунды
